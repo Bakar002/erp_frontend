939a6e1d677e8c60c2b613909ebe61f7f92dca7b
Bakar002 <abubakaramjad78600@gmail.com>
2024-05-23 11:44:11 +0500

    jdsbs

--- src/components/Admin/AdminAddCourse/AdminAddCourse.js
+++ src/components/Admin/AdminAddCourse/AdminAddCourse.js
@@ -20,7 +20,7 @@ export const AddCourse = () => {
   useEffect(() => {
     const fetchAllTeachers = async () => {
       try {
-        const response = await axios.get("https://belikeerp.onrender.com/load-all-teachers");
+        const response = await axios.get("https://belikeerp.onrender.com/api/v1/admin/load-all-teachers");
         setAllTeachers(response?.data?.teachers);
       } catch (error) {
         console.log(error?.response?.data?.message);
