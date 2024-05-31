04a4c2b7416ea7421875a69569d631e6cf62a4ec
Bakar002 <abubakaramjad78600@gmail.com>
2024-06-01 01:48:37 +0500

    sds

--- src/App.js
+++ src/App.js
@@ -53,6 +53,7 @@ import AdminFeedback from "./components/Admin/AdminFeedback/AdminFeedback.js";
 import TeacherAddResult  from "./components/Teacher/TeacherAddResult/TeacherAddResult.js";
 import AddResult from "./components/Student/StudentResult/StudentResult.js";
 import AdmissionForm from './components/Landing/AdmissionForm';
+import StudentLogin from './components/Student/StudentLogin/Login.Student';
 function App() {
 
 
@@ -150,7 +151,7 @@ function App() {
           <Route
             path="/student-time-table"
             element={
-              isStudentAuthenticated ? <StudentCourseTimetable /> : <Login />
+              isStudentAuthenticated ? <StudentCourseTimetable /> : <StudentLogin />
             }
           />
           <Route
