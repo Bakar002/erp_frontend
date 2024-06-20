a6816bf531987d48223d0f26b6ae0c2d08c500a7
Bakar002 <abubakaramjad78600@gmail.com>
2024-06-21 02:32:19 +0500

    ajd

--- src/components/Admin/AdminLogin/AdminLogin.js
+++ src/components/Admin/AdminLogin/AdminLogin.js
@@ -79,7 +79,7 @@ const AdminLogin = () => {
       if (response.status === 200) {
         handleShowSuccessToast(response.data.message);
         console.log(response.data);
-        navigate("/admin-dashboard");
+        navigate("/");
         window.location.reload(); // Reload the window after navigating
       } else {
         console.log("Unexpected response status:", response.status);
