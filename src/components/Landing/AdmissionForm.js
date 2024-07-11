fe7f073b46afb72b1cd2fce7ae45690d877cd355
Bakar002 <abubakaramjad78600@gmail.com>
2024-07-12 03:28:51 +0500

    ed

--- src/components/Landing/AdmissionForm.js
+++ src/components/Landing/AdmissionForm.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Toaster, toast } from "react-hot-toast";
 import ThreeDotLoader from "../Loaders/ThreeDotLoader";
 
-export const AdmissionForm = () => {
+const AdmissionForm = () => {
   const [studentName, setStudentName] = useState("");
   const [studentEmail, setStudentEmail] = useState("");
   const [studentPhone, setStudentPhone] = useState("");
@@ -12,8 +12,26 @@ export const AdmissionForm = () => {
   const [guardianName, setGuardianName] = useState("");
   const [guardianPhone, setGuardianPhone] = useState("");
   const [studentClass, setStudentClass] = useState("");
-  const [studentPhoto, setStudentPhoto] = useState(null);
+  const [studentIdPhoto, setStudentIdPhoto] = useState(null);
+  const [lastDegree, setLastDegree] = useState(null);
+  const [adminId, setAdminId] = useState("");
+  const [admins, setAdmins] = useState([]);
+  const [paymentMethod, setPaymentMethod] = useState("");
+  const [paymentSlip, setPaymentSlip] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchAdmins = async () => {
+      try {
+        const response = await axios.get("https://belikeerp-3.onrender.com/api/v1/admin/load-all-admins");
+        setAdmins(response.data.data);
+      } catch (error) {
+        toast.error("Failed to load admins");
+      }
+    };
+
+    fetchAdmins();
+  }, []);
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
@@ -26,7 +44,11 @@ export const AdmissionForm = () => {
       guardianName &&
       guardianPhone &&
       studentClass &&
-      studentPhoto
+      studentIdPhoto &&
+      lastDegree &&
+      adminId &&
+      paymentMethod &&
+      (paymentMethod !== "online" || paymentSlip)  // Ensure payment slip is provided for online payment method
     ) {
       const formData = new FormData();
       formData.append("studentName", studentName);
@@ -37,12 +59,18 @@ export const AdmissionForm = () => {
       formData.append("guardianName", guardianName);
       formData.append("guardianPhone", guardianPhone);
       formData.append("studentClass", studentClass);
-      formData.append("studentPhoto", studentPhoto);
+      formData.append("studentIdPhoto", studentIdPhoto);
+      formData.append("lastDegree", lastDegree);
+      formData.append("adminId", adminId);
+      formData.append("paymentMethod", paymentMethod);
+      if (paymentMethod === "online") {
+        formData.append("paymentSlip", paymentSlip);
+      }
 
       try {
         setLoading(true);
         const response = await axios.post(
-          "https://your-api-endpoint.com/api/v1/admissions",
+          "https://belikeerp-3.onrender.com/api/v1/student/admissionsubmit",
           formData,
           {
             headers: {
@@ -60,7 +88,11 @@ export const AdmissionForm = () => {
         setGuardianName("");
         setGuardianPhone("");
         setStudentClass("");
-        setStudentPhoto(null);
+        setStudentIdPhoto(null);
+        setLastDegree(null);
+        setAdminId("");
+        setPaymentMethod("");
+        setPaymentSlip(null);
       } catch (error) {
         toast.error(error.response.data.message);
         setLoading(false);
@@ -135,27 +167,22 @@ export const AdmissionForm = () => {
                   onChange={(e) => setStudentPhone(e.target.value)}
                 />
               </div>
-              
               <div className="md:w-[45%] ">
-                  <label
-                    htmlFor="studentDOB"
-                    className="leading-7 text-sm text-gray-600"
-                  >
-                    Student Dob
-                  </label>
-                  <input
-                    id="studentDOB"
-                    name="studentDOB"
-                    value={studentDOB}
-                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-purple-500 focus:bg-white focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-2 px-3 transition-colors duration-200 ease-in-out"
-                    onChange={(e) => setStudentDOB(e.target.value)}
-                  />
-                </div>
-
-
-
-
-
+                <label
+                  htmlFor="studentDOB"
+                  className="leading-7 text-sm text-gray-600"
+                >
+                  Student DOB
+                </label>
+                <input
+                  type="date"
+                  id="studentDOB"
+                  name="studentDOB"
+                  value={studentDOB}
+                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-purple-500 focus:bg-white focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-2 px-3 transition-colors duration-200 ease-in-out"
+                  onChange={(e) => setStudentDOB(e.target.value)}
+                />
+              </div>
             </div>
             <div>
               <label
@@ -228,28 +255,100 @@ export const AdmissionForm = () => {
             </div>
             <div>
               <label
-                htmlFor="studentPhoto"
+                htmlFor="studentIdPhoto"
                 className="leading-7 text-sm text-gray-600"
               >
-                Student Id card Photo
+                Student Photo
               </label>
               <input
                 type="file"
-                id="studentPhoto"
-                name="studentPhoto"
-                className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-purple-500 focus:bg-white focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-2 px-3 transition-colors duration-200 ease-in-out"
-                onChange={(e) => setStudentPhoto(e.target.files[0])}
+                id="studentIdPhoto"
+                name="studentIdPhoto"
+                className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-purple-500 focus:bg-white focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 transition-colors duration-200 ease-in-out"
+                onChange={(e) => setStudentIdPhoto(e.target.files[0])}
               />
             </div>
-          </div>
-          <div className="w-full text-center mt-6">
-            <button
-              className="w-1/2 md:w-1/3 lg:w-1/4 mx-auto text-white bg-blue-600 border-0 py-2 px-6 focus:outline-none hover:bg-purple-700 rounded text-lg"
-              type="submit"
-              disabled={loading}
-            >
-              {loading ? <ThreeDotLoader /> : "Submit"}
-            </button>
+            <div>
+              <label
+                htmlFor="lastDegree"
+                className="leading-7 text-sm text-gray-600"
+              >
+                Last Degree
+              </label>
+              <input
+                type="file"
+                id="lastDegree"
+                name="lastDegree"
+                className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-purple-500 focus:bg-white focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 transition-colors duration-200 ease-in-out"
+                onChange={(e) => setLastDegree(e.target.files[0])}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="paymentMethod"
+                className="leading-7 text-sm text-gray-600"
+              >
+                Payment Method
+              </label>
+              <select
+                id="paymentMethod"
+                name="paymentMethod"
+                value={paymentMethod}
+                className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-purple-500 focus:bg-white focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-2 px-3 transition-colors duration-200 ease-in-out"
+                onChange={(e) => setPaymentMethod(e.target.value)}
+              >
+                <option value="">Select Payment Method</option>
+                <option value="online">Online</option>
+                <option value="physical">Physical</option>
+              </select>
+            </div>
+            {paymentMethod === "online" && (
+              <div>
+                <label
+                  htmlFor="paymentSlip"
+                  className="leading-7 text-sm text-gray-600"
+                >
+                  Payment Slip
+                </label>
+                <input
+                  type="file"
+                  id="paymentSlip"
+                  name="paymentSlip"
+                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-purple-500 focus:bg-white focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 transition-colors duration-200 ease-in-out"
+                  onChange={(e) => setPaymentSlip(e.target.files[0])}
+                />
+              </div>
+            )}
+            <div>
+              <label
+                htmlFor="adminId"
+                className="leading-7 text-sm text-gray-600"
+              >
+                Admin
+              </label>
+              <select
+                id="adminId"
+                name="adminId"
+                value={adminId}
+                className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-purple-500 focus:bg-white focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-2 px-3 transition-colors duration-200 ease-in-out"
+                onChange={(e) => setAdminId(e.target.value)}
+              >
+                <option value="">Select Admin</option>
+                {admins.map((admin) => (
+                  <option key={admin._id} value={admin._id}>
+                    {admin.adminName}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="flex justify-center mt-6">
+              <button
+                type="submit"
+                className="text-white bg-purple-500 border-0 py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg"
+              >
+                {loading ? <ThreeDotLoader /> : "Submit"}
+              </button>
+            </div>
           </div>
         </div>
       </form>
