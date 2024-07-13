bcf6a32535e9645851edd209146285f2d9e94bf6
Bakar002 <abubakaramjad78600@gmail.com>
2024-07-13 15:30:09 +0500

    schools

--- src/components/Landing/SchoolsBlocks.js
+++ src/components/Landing/SchoolsBlocks.js
@@ -7,6 +7,10 @@ import schoolportallogo3 from "../../Assets/Img/s3.jpg"; // Replace with your ac
 import schoolportallogo4 from "../../Assets/Img/s4.jpg"; // Replace with your actual school portal logo path
 import schoolportallogo5 from "../../Assets/Img/s5.jpg"; // Replace with your actual school portal logo pathafaq.jpeg
 import schoolportallogo6 from "../../Assets/Img/afaq.jpeg"; // Replace with your actual school portal logo pathafaq.jpeg
+import schoolportallogo7 from "../../Assets/Img/UIES.jpg"; // Replace with your actual school portal logo pathafaq.jpeg
+import schoolportallogo8 from "../../Assets/Img/syed.jpg"; // Replace with your actual school portal logo pathafaq.jpeg
+
+
 
 
 
@@ -49,6 +53,16 @@ const SchoolBlocks = () => {
     {
       name: "Afaq School",
       logo: schoolportallogo6, // Replace with your actual school logo path
+      link: "/admin-login", // Replace with the actual login link
+    },
+    {
+      name: "Al Syed School/Academy",
+      logo: schoolportallogo8, // Replace with your actual school logo path
+      link: "/admin-login", // Replace with the actual login link
+    },
+    {
+      name: "UIES Tajbagh Chapter Academy",
+      logo: schoolportallogo7, // Replace with your actual school logo path
       link: "/admin-login", // Replace with the actual login link
     },
   ];
